e724e181a0681fc8e89f5c2d151494d76c045aa1
rajeshrj1991 <rajeshrajendran.j92@gmail.com>
2026-07-06 16:09:13 +0530

    Added methods and Class

---
+++ src/test/java/com/hotelbookbasic/NewClass.java
@@ -0,0 +1,13 @@
+package com.hotelbookbasic;
+
+public class NewClass {
+	private void python() {
+		// TODO Auto-generated method stub
+
+	}
+	private void visual() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
